4e434fc8e6d890421c6971465d624c5b759504cc
sandhisarkar <sandhisarkar2@gmail.com>
2022-03-21 23:49:29 +0530

    first commit

---
+++ FileFolderDel/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace FileFolderDel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "All",
+            "Temp",
+            "Prefetch",
+            "%Temp%",
+            "Recycle Bin"});
+            this.comboBox1.Location = new System.Drawing.Point(37, 90);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(215, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.Leave += new System.EventHandler(this.comboBox1_Leave);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(97, 158);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 28);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Run";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(46, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(189, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Commands to Execute :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(288, 258);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cache Cleaner";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ FileFolderDel/Form1.cs
@@ -0,0 +1,335 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace FileFolderDel
+{
+    public partial class Form1 : Form
+    {
+        public string path;
+        public string path1;
+        public string path2;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        enum RecycleFlags : uint
+        {
+            SHRB_NOCONFIRMATION = 0x00000001,
+            SHRB_NOPROGRESSUI = 0x00000002,
+            SHRB_NOSOUND = 0x00000004
+        }
+        [DllImport("Shell32.dll", CharSet = CharSet.Unicode)]
+        static extern uint SHEmptyRecycleBin(IntPtr hwnd, string pszRootPath, RecycleFlags dwFlags);
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            //with network: format : Networkname\Username 
+            string userName = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+
+            //name without network: format: Username
+            string name = Environment.UserName;
+
+        }
+
+        private void comboBox1_Leave(object sender, EventArgs e)
+        {
+           // MessageBox.Show(comboBox1.SelectedText.ToString());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string textcombo = comboBox1.Text.ToString();
+           // MessageBox.Show(comboBox1.Text.ToString());
+
+           
+
+            if(textcombo == "Temp")
+            {
+                path = @"C:\Windows\Temp";
+
+                System.IO.DirectoryInfo di = new DirectoryInfo(path);
+
+                if (di.Exists)
+                {
+                    string ok = "OKAY";
+                }
+                else
+                {
+                    string ok = "SORRY";
+                }
+
+                foreach (FileInfo file in di.GetFiles())
+                {
+                    try
+                    {
+                        file.Delete();
+                    }
+                    catch (Exception ex)
+                    {
+                        //MessageBox.Show(ex.ToString());
+                        continue;
+                    }
+                }
+                foreach (DirectoryInfo dir in di.GetDirectories())
+                {
+                    try
+                    {
+                        dir.Delete(true);
+                    }
+                    catch (Exception ex1)
+                    {
+                        //MessageBox.Show(ex1.ToString());
+                        continue;
+                    }
+                }
+            }
+            if (textcombo == "Prefetch")
+            {
+                path = @"C:\Windows\Prefetch";
+
+                System.IO.DirectoryInfo di = new DirectoryInfo(path);
+
+                if (di.Exists)
+                {
+                    string ok = "OKAY";
+                }
+                else
+                {
+                    string ok = "SORRY";
+                }
+
+                foreach (FileInfo file in di.GetFiles())
+                {
+                    try
+                    {
+                        file.Delete();
+                    }
+                    catch (Exception ex)
+                    {
+                        //MessageBox.Show(ex.ToString());
+                        continue;
+                    }
+                }
+                foreach (DirectoryInfo dir in di.GetDirectories())
+                {
+                    try
+                    {
+                        dir.Delete(true);
+                    }
+                    catch (Exception ex1)
+                    {
+                        //MessageBox.Show(ex1.ToString());
+                        continue;
+                    }
+                }
+            }
+            if (textcombo == "%Temp%")
+            {
+                path = @"C:\Users\" + Environment.UserName + "\\AppData\\Local\\Temp";
+
+                System.IO.DirectoryInfo di = new DirectoryInfo(path);
+
+                if (di.Exists)
+                {
+                    string ok = "OKAY";
+                }
+                else
+                {
+                    string ok = "SORRY";
+                }
+
+                foreach (FileInfo file in di.GetFiles())
+                {
+                    try
+                    {
+                        file.Delete();
+                    }
+                    catch (Exception ex)
+                    {
+                        //MessageBox.Show(ex.ToString());
+                        continue;
+                    }
+                }
+                foreach (DirectoryInfo dir in di.GetDirectories())
+                {
+                    try
+                    {
+                        dir.Delete(true);
+                    }
+                    catch (Exception ex1)
+                    {
+                        //MessageBox.Show(ex1.ToString());
+                        continue;
+                    }
+                }
+            }
+            if (textcombo == "Recycle Bin")
+            {
+                DialogResult result;
+                result = MessageBox.Show("Are you sure to delete all items in recycle bin", "Empty Recycle bin", MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+                    try
+                    {
+                        uint IsSuccess = SHEmptyRecycleBin(IntPtr.Zero, null, RecycleFlags.SHRB_NOCONFIRMATION);
+                        MessageBox.Show("Empty the RecycleBin successsfully", "Empty the RecycleBin", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Empty the RecycleBin failed" + ex.Message, "Empty the RecycleBin", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                        Application.Exit();
+                    }
+                }
+            }
+
+            if (textcombo == "All")
+            {
+                path = @"C:\Windows\Temp";
+
+                System.IO.DirectoryInfo di = new DirectoryInfo(path);
+
+                if (di.Exists)
+                {
+                    string ok = "OKAY";
+                }
+                else
+                {
+                    string ok = "SORRY";
+                }
+
+                foreach (FileInfo file in di.GetFiles())
+                {
+                    try
+                    {
+                        file.Delete();
+                    }
+                    catch (Exception ex)
+                    {
+                        //MessageBox.Show(ex.ToString());
+                        continue;
+                    }
+                }
+                foreach (DirectoryInfo dir in di.GetDirectories())
+                {
+                    try
+                    {
+                        dir.Delete(true);
+                    }
+                    catch (Exception ex1)
+                    {
+                        //MessageBox.Show(ex1.ToString());
+                        continue;
+                    }
+                }
+
+                path1 = @"C:\Windows\Prefetch";
+
+                System.IO.DirectoryInfo di1 = new DirectoryInfo(path1);
+
+                if (di1.Exists)
+                {
+                    string ok = "OKAY";
+                }
+                else
+                {
+                    string ok = "SORRY";
+                }
+
+                foreach (FileInfo file1 in di1.GetFiles())
+                {
+                    try
+                    {
+                        file1.Delete();
+                    }
+                    catch (Exception ex)
+                    {
+                        //MessageBox.Show(ex.ToString());
+                        continue;
+                    }
+                }
+                foreach (DirectoryInfo dir1 in di1.GetDirectories())
+                {
+                    try
+                    {
+                        dir1.Delete(true);
+                    }
+                    catch (Exception ex1)
+                    {
+                        //MessageBox.Show(ex1.ToString());
+                        continue;
+                    }
+                }
+
+                path2 = @"C:\Users\" + Environment.UserName + "\\AppData\\Local\\Temp";
+
+                System.IO.DirectoryInfo di2 = new DirectoryInfo(path2);
+
+                if (di2.Exists)
+                {
+                    string ok = "OKAY";
+                }
+                else
+                {
+                    string ok = "SORRY";
+                }
+
+                foreach (FileInfo file2 in di2.GetFiles())
+                {
+                    try
+                    {
+                        file2.Delete();
+                    }
+                    catch (Exception ex)
+                    {
+                        //MessageBox.Show(ex.ToString());
+                        continue;
+                    }
+                }
+                foreach (DirectoryInfo dir2 in di2.GetDirectories())
+                {
+                    try
+                    {
+                        dir2.Delete(true);
+                    }
+                    catch (Exception ex1)
+                    {
+                        //MessageBox.Show(ex1.ToString());
+                        continue;
+                    }
+                }
+
+
+                DialogResult result;
+                result = MessageBox.Show("Are you sure to delete all items in recycle bin", "Empty Recycle bin", MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+                    try
+                    {
+                        uint IsSuccess = SHEmptyRecycleBin(IntPtr.Zero, null, RecycleFlags.SHRB_NOCONFIRMATION);
+                        MessageBox.Show("Empty the RecycleBin successsfully", "Empty the RecycleBin", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Empty the RecycleBin failed" + ex.Message, "Empty the RecycleBin", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                        Application.Exit();
+                    }
+                }
+
+            }
+
+            
+
+
+            MessageBox.Show("Process is completed successfully...");
+        }
+    }
+}
